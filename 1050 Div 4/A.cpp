53448742462eff0916cf1502fc27fe7e766c6f90
Nusrat Jahan Raisa <jahanthinks@gmail.com>
2025-09-14 12:53:00 +0600

    SolvedA.cpp

--- 1050 Div 4/A.cpp
+++ 1050 Div 4/A.cpp
@@ -1,1 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define endl '\n'
+#define yes cout<<"YES\n";
+#define no cout<<"NO\n";
+#define optimize() ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
 
+
+
+int main(){ 
+    optimize();  
+    
+    int t;cin>>t;
+    while(t--){
+        int x,n;cin>>x>>n;
+        int sum=0;
+        for(int i=1;i<=n;i++)
+        {
+            /* code */
+            if(i%2==1) sum+=x;
+            else sum-=x;
+        }
+        cout<<sum<<endl;
+    }
+
+    return 0;
+}
